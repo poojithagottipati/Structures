83073ea7e6a3cdb3a71c681e8d025ce38f5101a1
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-31 18:47:40 +0530

    Create Little&BigEndian.c

---
+++ Little&BigEndian.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num = 1;
+    char *byte = (char *)&num;
+    if (*byte == 1) {
+        printf("The platform is Little Endian.\n");
+    } else {
+        printf("The platform is Big Endian.\n");
+    }
+
+    return 0;
+}
+/*
+The platform is Little Endian.
+*/
